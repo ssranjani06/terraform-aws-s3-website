722fd8ce24da1b830b70f4b81a3455d71fc0a814
Sivaranjani Saravanan <ssranjani06@gmail.com>
2022-10-02 15:37:07 +0530

    first commits

--- c3-s3bucket.tf
+++ c3-s3bucket.tf
@@ -1,7 +1,7 @@
 # Call our Custom Terraform Module which we built earlier
 module "s3-website" {
   source  = "app.terraform.io/sivaranjani-digiinfo/s3-website/aws"
-  version = "1.0.0"
+  version = "1.0.2"
   # insert required variables here
   bucket_name = var.my_s3_bucket
   tags        = var.my_s3_tags
