0ce4d3319e59007cde478b4d74309cedb59e91c5
ssranjani06 <99155402+ssranjani06@users.noreply.github.com>
2022-10-08 15:08:51 +0530

    Update c3-s3bucket.tf

--- c3-s3bucket.tf
+++ c3-s3bucket.tf
@@ -1,9 +1,8 @@
 # Call our Custom Terraform Module which we built earlier
 module "s3-website" {
-  source  = "app.terraform.io/sivaranjani-digiinfo/s3-website/aws"
-  version = "1.0.3"
+  source  = "app.terraform.io/ranjani-hcta-demo1/s3-website/aws"
+  version = "1.0.4"
   # insert required variables here
   bucket_name = var.my_s3_bucket
-  tags        = var.my_s3_tags
+  tags = var.my_s3_tags
 }
-
